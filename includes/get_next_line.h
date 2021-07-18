83e4f69ddeac4fcd8d8971fde13f827fe6916465
gmorros <gmorros@student.21-school.ru>
2021-07-18 11:20:04 +0300

    google code style

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int		get_next_line(int fd, char **line);
-int		ft_remainder(char **line, char **remainder, char **n_here);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s);
-size_t	ft_strlen(const char *s);
+int get_next_line(int fd, char **line);
+int ft_remainder(char **line, char **remainder, char **n_here);
+char *ft_strchr(const char *s, int c);
+char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strdup(const char *s);
+size_t ft_strlen(const char *s);
 
 #	endif
